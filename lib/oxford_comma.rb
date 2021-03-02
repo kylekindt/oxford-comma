1f493f6314baeaee3eda2a149a41fd6ee246e5ba
Kyle Kindt <k.kindt8@gmail.com>
2021-03-02 14:46:08 -0500

    commit tree 4c8c892939856843b02a27a3f19c9e96f2b96991

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require "pry"
 def oxford_comma(array)
+    if array.length == 1
+        return array[0]
+    elsif array.size == 2
+        return array.join(" and ")
+    # elsif array.length == 3
+        # array[2] = "and #{array[2]}"
+        # return array.join(", ")   
+    else array.length >= 3
+        array[-1] = "and #{array[-1]}"
+        return array.join(", ")
+    end
 
 end
